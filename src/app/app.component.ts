78d670d3c08428aa19bfc100769c49155841e37a
anhvupt <45546183+anhvupt@users.noreply.github.com>
2023-05-10 23:35:19 +0700

    update layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {Survey} from "../types/Survey";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Survey } from '../types/Survey';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
@@ -11,61 +11,62 @@ interface IParams {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   statuses: string[] = ['All', 'Active', 'Completed'];
   categories: string[] = ['Development', 'Workplace', 'Hardware'];
   status = 'status';
-  category = "category";
+  category = 'category';
 
-  paramsSubject = new BehaviorSubject<IParams>({category: '', status: ''});
+  paramsSubject = new BehaviorSubject<IParams>({ category: '', status: '' });
   destroySubject = new Subject<unknown>();
   params$ = this.paramsSubject.asObservable();
 
   surveyList: Survey[] = [
     {
-      title: "Designer Survey",
-      category: "Workplace",
-      status: "Active",
-      label: "New Framework",
+      title: 'Designer Survey',
+      category: 'Workplace',
+      status: 'Active',
+      label: 'New Framework',
     },
     {
-      title: "Developer Survey",
-      category: "Development",
-      status: "Active",
-      label: "Education",
+      title: 'Developer Survey',
+      category: 'Development',
+      status: 'Active',
+      label: 'Education',
     },
     {
-      title: "Backend Survey",
-      category: "Hardware",
-      status: "Completed",
-      label: "Personal",
-    }
-  ]
+      title: 'Backend Survey',
+      category: 'Hardware',
+      status: 'Completed',
+      label: 'Personal',
+    },
+  ];
 
   filteredList: Survey[];
 
   ngOnInit() {
-    this.params$.pipe(takeUntil(this.destroySubject)).subscribe(params => {
-      const hasStatus = (x: Survey) => 
-      (!params.status || params.status === 'All') 
-      ? true 
-      : x.status === params.status;
-      const hasCategory = (x: Survey) => !params.category ? true : x.category === params.category;
-      this.filteredList = this.surveyList.filter(x => hasCategory(x) && hasStatus(x))
-    })
+    this.params$.pipe(takeUntil(this.destroySubject)).subscribe((params) => {
+      const hasStatus = (x: Survey) =>
+        !params.status || params.status === 'All'
+          ? true
+          : x.status === params.status;
+      const hasCategory = (x: Survey) =>
+        !params.category ? true : x.category === params.category;
+      this.filteredList = this.surveyList.filter(
+        (x) => hasCategory(x) && hasStatus(x)
+      );
+    });
   }
 
   onFilterSelected(filter: string, type: keyof IParams) {
-    const params = {...this.paramsSubject.value, [type]: filter};
+    const params = { ...this.paramsSubject.value, [type]: filter };
     this.paramsSubject.next(params);
   }
 
   ngOnDestroy(): void {
-    this.destroySubject.next();
+    this.destroySubject.next(null);
     this.destroySubject.complete();
   }
-
-
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {applyPolyfills, defineCustomElements} from "h8k-components/loader";
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,10 +7,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
-applyPolyfills()
-  .then(() => {
-    defineCustomElements()
-  })
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
