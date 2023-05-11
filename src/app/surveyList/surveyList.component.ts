5fac008a7274ba78a01e446637914751ce704b2b
anhvupt <45546183+anhvupt@users.noreply.github.com>
2023-05-11 14:27:55 +0700

    1 file modified

--- src/app/surveyList/surveyList.component.ts
+++ src/app/surveyList/surveyList.component.ts
@@ -1,18 +1,13 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {Survey} from "../../types/Survey";
+import { Component, OnInit, Input } from '@angular/core';
+import { Survey } from '../../types/Survey';
 
 @Component({
   selector: 'survey-list',
   templateUrl: './surveyList.component.html',
-  styleUrls: ['./surveyList.component.scss']
+  styleUrls: ['./surveyList.component.scss'],
 })
-
 export class SurveyList implements OnInit {
   @Input() surveyList: Survey[];
 
-  ngOnInit() {
-
-  }
-
-  
+  ngOnInit() {}
 }
